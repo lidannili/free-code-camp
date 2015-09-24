478eae085a89e997eea46422d37248f248f1ccb1
Danni <lidannili@gmail.com>
2015-09-23 21:24:23 -0500

    Create 9.Bonfire- Boo who.js

---
+++ 7. Intermediate Algorithm Scripting/9.Bonfire- Boo who.js
@@ -0,0 +1,14 @@
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if(bool===true||bool==false){
+  return true;
+  }
+  else{
+    return false;
+  }
+}
+
+boo(null);
+
+//Boolean Objects Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+// Syntax: new Boolean([value])
